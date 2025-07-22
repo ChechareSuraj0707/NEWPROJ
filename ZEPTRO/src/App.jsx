627c9bacf48b2813c03e516003f74aa260bd3bd6
admin <admin@admins-Mac-mini.example.com>
2025-07-22 12:03:17 +0530

    16.18 min

--- ZEPTRO/src/App.jsx
+++ ZEPTRO/src/App.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return(
-    <div className='text-red-500 text-4x1'>
-Hey this is Suraj 
-    </div>
-  )
-}
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/product" element={<Product />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
