2c1e5db71413b3e9724df4fe22121aefe0921a91
admin <admin@admins-Mac-mini.example.com>
2025-09-17 12:43:19 +0530

    4.42.27

--- ZEPTRO/src/pages/Cart.jsx
+++ ZEPTRO/src/pages/Cart.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { useCart } from "../context/CartContext";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { LuNotebookText } from "react-icons/lu";
+import { MdDeliveryDining } from "react-icons/md";
+import { GiShoppingBag } from "react-icons/gi";
 
 const Cart = () => {
   const { cartItem } = useCart();
+  const totalPrice = cartItem.reduce((total, item) => total + item.price, 0);
   return (
     <div className="mt-10 max-w-6xl mx-auto mb-5">
       {cartItem.length > 0 ? (
@@ -43,6 +47,133 @@ const Cart = () => {
                   </div>
                 );
               })}
+            </div>
+            <div className="grid grid-cols-2 gap-20">
+              <div className=" bg-gray-100 rounded-md p-7 mt-4 space-y-2">
+                <h1 className="text-gray-800 font-bold text-xl">
+                  Delivery Info
+                </h1>
+                <div className="flex flex-col space-y-1">
+                  <label htmlFor=""> Full Name</label>
+                  <input
+                    type="text"
+                    placeholder="Enter your name"
+                    className="p-2 rounded-md"
+                  />
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <label htmlFor=""> Address</label>
+                  <input
+                    type="text"
+                    placeholder="Enter your address"
+                    className="p-2 rounded-md"
+                  />
+                </div>
+                <div className="flex w-full gap-5">
+                  <div className="flex flex-col space-y-1 w-full">
+                    <label htmlFor=""> State</label>
+                    <input
+                      type="text"
+                      placeholder="Enter your state"
+                      className="p-2 rounded-md w-full"
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-1 w-full">
+                    <label htmlFor=""> PostCode</label>
+                    <input
+                      type="text"
+                      placeholder="Enter your postcode"
+                      className="p-2 rounded-md w-full"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col space-y-1 w-full">
+                  <label htmlFor=""> Country</label>
+                  <input
+                    type="text"
+                    placeholder="Enter your country"
+                    className="p-2 rounded-md w-full"
+                  />
+                </div>
+                <div className="flex flex-col space-y-1 w-full">
+                  <label htmlFor=""> Phone No</label>
+                  <input
+                    type="text"
+                    placeholder="Enter your number"
+                    className="p-2 rounded-md w-full"
+                  />
+                </div>
+                <button className="bg-red-500 text-white px-3 py-1 rounded-md mt-3 cursor-pointer">
+                  Submit
+                </button>
+                <div className="flex items-center justify-center w-full text-gray-700">
+                  -----------------OR------------------
+                </div>
+                <div className="flex justify-center">
+                  <button className="bg-red-500 text-white px-3 py-2 rounded-md">
+                    {" "}
+                    Detect location
+                  </button>
+                </div>
+              </div>
+
+              <div className="bg-white border border-gray-100 shadow-xl rounded-md p-7 mt-4 space-y-2 h-max">
+                <h1 className="text-gray-800 font-bold text-xl">
+                  Bill Details
+                </h1>
+                <div className="flex justify-between items-center">
+                  <h1 className="flex gap-1 item-center text-gray-700">
+                    <span>
+                      <LuNotebookText></LuNotebookText>
+                    </span>
+                    Items total
+                  </h1>
+                  <p>${totalPrice}</p>
+                </div>
+                <div className="flex justify-between items-center">
+                  <h1 className="flex gap-1 item-center text-gray-700">
+                    <span>
+                      <MdDeliveryDining />
+                    </span>
+                    Delivery Charges
+                  </h1>
+                  <p className="text-red-500 font-semibold">
+                    <span className="text-gray-600 line-through">$25</span>Free
+                  </p>
+                </div>
+                <div className="flex justify-between items-center">
+                  <h1 className="flex gap-1 item-center text-gray-700">
+                    <span>
+                      <GiShoppingBag />
+                    </span>
+                    Handling Charges
+                  </h1>
+                  <p className="text-red-500 font-semibold">$5</p>
+                </div>
+                <hr className="text-gray-200 mt-2" />
+                <div className="flex justify-between items-center">
+                  <h1 className="font-semibold text-lg"> Grand total</h1>
+                  <p className="font-semibold text-lg">${totalPrice + 5}</p>
+                </div>
+                <div>
+                  <h1 className="font-semibold text-gray-700 mb-3 mt-7">
+                    Apply Promo Code
+                  </h1>
+                  <div className="flex gap-3">
+                    <input
+                      type="text"
+                      placeholder="Enter code"
+                      className="p-2 rounded-md w-full"
+                    />
+                    <button className="bg-white text-black border border-gray-200 px-4 cursor-pointer py-1 rounded-md">
+                      Apply
+                    </button>
+                  </div>
+                </div>
+                <button className="bg-red-500 text-white px-3 py-2 rounded-md cursor-pointer mt-3 w-full">
+                  Proceed to Checkout
+                </button>
+              </div>
             </div>
           </div>
         </div>
